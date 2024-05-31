4770c10acbfddba75359fed59c6f54e09b8ebd12
SouthernMinami <kanou373@gmail.com>
2024-05-31 21:41:04 +0900

    Fix: composerのオートローダー設定を修正、フォームボタンのtypeを削除

--- Views/component/form.php
+++ Views/component/form.php
@@ -1,8 +1,8 @@
-<form id="post-form" action="/" method="POST">
+<form id="post-form" method="POST">
     <div class="form-group">
         <label for="title">Snippet Title</label>
         <input type="text" class="form-control" id="title" name="title" required>
         <label for="expiration">Expiration Date</label>
         <input type="date" class="form-control" id="expiration" name="expiration" required>
-        <input id="post-btn" type="post" class="btn btn-success" value="Post Snippet">
+        <input id="post-btn" class="btn btn-success" value="Post Snippet">
     </div>
