84127796c072e794b11c91c204cc2b27623dfcb9
SouthernMinami <kanou373@gmail.com>
2024-07-04 08:23:00 +0900

    Chore: execSeedCmd.php動作確認用のecho

--- Helpers/execSeedCmd.php
+++ Helpers/execSeedCmd.php
@@ -18,6 +18,7 @@ $expirationDate = ValidationHelper::string($input['expirationDate'] ?? null);
 
 $dataStr = implode(',', [$title, $language, $content, $path, $expirationDate]);
 
+echo $content . ', ' . $date . ', ' . $path . ', ' . $expirationDate . ',' . PHP_EOL;
 $command = sprintf('php ../console seed --data %s', $dataStr);
 exec($command, $output, $return_var);
 
