629c5121e86486d4e0f30d3f700e69c25907ceb7
SouthernMinami <kanou373@gmail.com>
2024-09-09 15:42:01 +0900

    Fix: adjusted style

--- Views/component/form.php
+++ Views/component/form.php
@@ -2,7 +2,7 @@
     <div class="form-group">
         <label for="title">Snippet Title</label>
         <input type="text" class="form-control" id="title" name="title" required>
-        <label for="expiration">Expiration Date</label>
+        <label for="expiration" class="pt-2">Expiration Date</label>
         <input type="date" class="form-control" id="expiration" name="expiration" required>
-        <input id="post-btn" class="btn btn-success" value="Post Snippet">
+        <input id="post-btn" class="btn btn-success mt-2" value="Post Snippet">
     </div>
